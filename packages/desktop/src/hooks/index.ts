8ca5f0a2c3227ec14f15625259552710260d5490
Carlos Miguel <carlos.miguel.oliveira.17@gmail.com>
2020-09-10 05:25:27 -0300

    changing a window management structure

--- packages/desktop/src/hooks/index.ts
+++ packages/desktop/src/hooks/index.ts
@@ -1,7 +1,9 @@
 import { useConfig } from './useConfig'
+import { useWindows } from './useWindows'
 import { useWindowSize } from './useWindowSize'
 
 export default {
   useConfig,
+  useWindows,
   useWindowSize
 }
