7d7dc1c48ea90b44430590f7c7c09275c35157f3
Carlos Miguel <carlos.miguel.oliveira.17@gmail.com>
2020-09-10 17:59:22 -0300

    updating: input component files

--- packages/desktop/src/components/Form/Input/index.tsx
+++ packages/desktop/src/components/Form/Input/index.tsx
@@ -1,36 +1,46 @@
-import React, { InputHTMLAttributes, useRef, useEffect } from 'react'
+import React, { InputHTMLAttributes, useRef, useEffect, useState } from 'react'
 import { useField } from '@unform/core'
 
-import { Container } from './styles'
+import { Container, Eye, EyeOff, InputGroup } from './styles'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  flex?: number;
+  type?: string;
   name: string;
   label?: string;
+  ref?: any;
 }
 
-const Input: React.FC<InputProps> = ({ label, name, ...rest }) => {
+const Input: React.FC<InputProps> = ({ flex, name, label, ...props }) => {
   const inputRef = useRef<HTMLInputElement>(null)
-  const { fieldName, registerField, defaultValue, error } = useField(name)
+  // const { fieldName, registerField, defaultValue, error } = useField(name)
+  const [type, setType] = useState(props.type)
+  const isPassword = props.type === 'password'
 
   useEffect(() => {
     if (inputRef.current) {
-      registerField({
-        name: fieldName,
-        path: 'value',
-        ref: inputRef.current
-      })
+      // registerField({
+      //   name: fieldName,
+      //   path: 'value',
+      //   ref: inputRef.current
+      // })
     }
   }, [])
 
   return (
-    <Container>
-      { label && <label htmlFor={fieldName}>{label}</label> }
+    <Container isPassword={isPassword} className="input-block" style={{ flex }}>
 
+      {label && <label htmlFor={name}>{label}</label> }
       <input
         ref={inputRef}
-        defaultValue={defaultValue}
-        {...rest}
-      />
+        // defaultValue={defaultValue}
+        {...{ ...props, ...{ type, id: name } }} />
+      { isPassword
+        ? type === 'password'
+          ? <Eye onClick={() => setType('text')}/>
+          : <EyeOff onClick={() => setType('password')}/>
+        : null
+      }
     </Container>
   )
 }
--- packages/desktop/src/components/Form/Input/styles.ts
+++ packages/desktop/src/components/Form/Input/styles.ts
@@ -1,46 +1,95 @@
 import styled from 'styled-components'
+import { darken, rgba } from 'polished'
 
-export const Container = styled.div`
-  flex: 1;
-  margin-top: 16px;
+import { FiEye, FiEyeOff } from 'react-icons/fi'
 
-  &:first-child {
-    margin: 0;
-  }
-
+interface Props {
+  isPassword: boolean
+}
+export const Container = styled.div<Props>`
+  width: 100%;
+  margin-bottom: 24px;
   label {
-    display: block;
-    color: ${props => props.theme.colors.white};
-    font-size: 14px;
-    font-weight: 600;
-    margin-bottom: 8px;
+    color: ${props => props.theme.colors.themeColors.text.light};
+    font-size: 1rem;
   }
+  position: relative;
 
   input {
-    height: 42px;
-    border-radius: 4px;
     width: 100%;
-    border: 0;
-    font-size: 16px;
-    color: ${props => props.theme.colors.white};
-    padding: 0 16px;
-    background: ${props => props.theme.colors.background.darkest};
-    border: 1px solid ${props => props.theme.colors.background.lighter};
+    height: 3rem;
+    margin-top: 0.8rem;
+    padding: 0 1.6rem;
 
-    transition: border 0.2s;
+    background: ${props => props.theme.colors.themeColors.tertiary};
+    color: ${props => props.theme.colors.themeColors.text.normal};
+    border-radius: 0.8rem;
+    border: 1px solid ${props => darken(0.1, props.theme.colors.themeColors.tertiary)};
+    box-shadow: 0px 13px 7px -10px ${props => rgba(darken(0.1, props.theme.colors.themeColors.tertiary), 0.4)};
 
-    &:focus {
-      border-color: ${props => props.theme.colors.purple};
-      outline: 0;
+    outline: 0;
+    font: 1rem Archivo;
+  }
+
+  &:focus-within::after {
+    width: calc(100% - 3.2rem);
+    height: 2px;
+    content: '';
+    background: ${props => props.theme.colors.themeColors.primary.light};
+    position: absolute;
+    left: 1.6rem;
+    right: 1.6rem;
+    bottom: 0;
+  }
+  & + & {
+    margin-top: 1.4rem;
+  }
+  @media (min-width: 700px) {
+    & + & {
+      margin-top: 0;
     }
+  }
+`
 
-    ::placeholder {
-      color: ${({ theme }) => theme.colors.opaque};
-    }
+export const Eye = styled(FiEye)`
+  position: absolute;
+  width: 1rem;
+  height: 1rem;
+  top: 60%;
+  right: 6%;
 
-    &:disabled {
-      cursor: not-allowed;
-      color: ${({ theme }) => theme.colors.opaque};
-    }
+  cursor: pointer;
+  color: ${({ theme }) => theme.colors.themeColors.text.light};
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.themeColors.primary.normal};
+  }
+`
+export const EyeOff = styled(FiEyeOff)`
+  position: absolute;
+  width: 1rem;
+  height: 1rem;
+  top: 60%;
+  right: 6%;
+
+  cursor: pointer;
+  color: ${({ theme }) => theme.colors.themeColors.text.light};
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.themeColors.primary.normal};
+  }
+`
+
+export const InputGroup = styled.div`
+  width: 100%;
+  height: auto;
+
+  display: flex;
+  column-gap: 1.6rem;
+
+  align-items: center;
+
+  @media (max-width: 750px) {
+   flex-direction: column
   }
 `
