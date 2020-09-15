7d021c4c401ae7b0751f215ca1e0e8c60752c8c8
Carlos Miguel <carlos.miguel.oliveira.17@gmail.com>
2020-09-15 07:24:14 -0300

    ending: layout of the search box

--- packages/desktop/src/components/Search/styles.ts
+++ packages/desktop/src/components/Search/styles.ts
@@ -49,7 +49,7 @@ export const Box = styled.div`
   box-shadow: 0px 2px 20px ${props => darken(0.16, props.theme.colors.themeColors.tertiary)};
   backdrop-filter: blur(6px);
 
-  z-index: 99999;
+  z-index: 999;
 
   header {
     width: 100%;
@@ -72,6 +72,7 @@ export const FilterOptions = styled.span<FilterOptionsProps>`
   cursor: pointer;
 
   font-weight: 500;
+  font-size: 0.8rem;
   ${({ actived, theme }) => actived
     ? css`
       color: ${theme.colors.themeColors.primary.normal};
@@ -86,4 +87,60 @@ export const FilterOptions = styled.span<FilterOptionsProps>`
 
 
 
+`
+
+export const Ebook = styled.div`
+  width: 100%;
+  height: 130px;
+  margin-top: .7rem;
+  margin-bottom: .7rem;
+  padding-bottom: 10px;
+
+  border-bottom: 2px solid ${props => rgba(props.theme.colors.themeColors.tertiary, 0.8)};
+
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: start;
+
+  &:last-child{
+    border: none;
+  }
+
+  .cover {
+    height: 100%;
+  }
+
+  > div {
+    width: 100%;
+    padding-left: 6%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: start;
+
+
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+
+    span {
+      font-size: .7rem;
+      font-weight: 600;
+
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      svg {
+        margin-right: 6px;
+      }
+    }
+
+    legend {
+      font-size: .7rem;
+    }
+  }
 `
