de50ba11032b336c38859d87bbda96a2c5c8e869
Carlos Miguel <carlos.miguel.oliveira.17@gmail.com>
2020-10-05 09:35:07 -0300

    update: disabling eslint @ typescript-eslint / no-var-requires rule

--- packages/eslint-config/.eslintrc.js
+++ packages/eslint-config/.eslintrc.js
@@ -28,6 +28,7 @@ module.exports = {
     "react/prop-types": "off",
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
+    "@typescript-eslint/no-var-requires": 'off',
     "import/extensions": [
       "error",
       "ignorePackages",
--- packages/server/jest.config.js
+++ packages/server/jest.config.js
@@ -1,7 +1,7 @@
-const { name } = require('./package.json');
+const { name } = require('./package.json')
 
 module.exports = {
   displayName: name,
   name,
   preset: 'ts-jest'
-};
+}
