ee6753777b08a5ed44cd5f5d97bb8b5cff642337
Carlos Miguel <carlos.miguel.oliveira.17@gmail.com>
2020-10-27 07:11:15 -0300

    adding: pdf preview in the dropZone component

--- packages/desktop/src/components/DropZone/index.tsx
+++ packages/desktop/src/components/DropZone/index.tsx
@@ -1,8 +1,10 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useState, useEffect, useRef, useContext } from 'react'
+import { ThemeContext } from 'styled-components'
+
+import WebViewer from '@pdftron/webviewer'
 import { useDropzone } from 'react-dropzone'
 import { FiUpload } from 'react-icons/fi'
-
-import { Container, P } from './styles'
+import { Container, P, WebViewerCSSVariables } from './styles'
 
 interface Props{
   text?: string;
@@ -11,14 +13,57 @@ interface Props{
 }
 
 const DropZone: React.FC<Props> = ({ onFileUploaded, text, accept }) => {
-  const [selectedFileUrl, setSelectedFileUrl] = useState('')
+  const webViewerRef = useRef<HTMLDivElement>(null)
+  const theme = useContext(ThemeContext)
+  const [selectedFileUrl, setSelectedFileUrl] = useState<File | null>(null)
+
+  useEffect(() => {
+    if (selectedFileUrl) {
+      SetNewEbook(selectedFileUrl)
+    }
+  }, [selectedFileUrl])
+
+  const SetNewEbook = (Ebook: File) => {
+    const webViewer = webViewerRef.current
+    if (!webViewer) return
+
+    if (webViewer.childNodes[0]) webViewer.removeChild(webViewer.childNodes[0])
+
+    const NewInstance = document.createElement('div')
+
+    WebViewer(
+      {
+        path: '/webviewer/lib'
+      },
+      NewInstance
+    ).then((instance) => {
+      instance.loadDocument(Ebook, { filename: 'preview.pdf' })
+
+      const { setTheme, iframeWindow, disableElements } = instance
+      const iframeDoc = iframeWindow.document
+      const Body = iframeDoc.querySelector('body')
+
+      disableElements(['header'])
+      disableElements(['toolsHeader'])
+
+      setTheme('dark')
+      // setTheme({ primary: 'blue', secondary: 'white' });
+      if (Body) {
+        Body.style = WebViewerCSSVariables(theme)
+      }
+    })
+
+    webViewer.appendChild(NewInstance)
+  }
 
   const onDrop = useCallback(acceptedFiles => {
     // Como so terá um arquivo, a imagem sempre estará na pos 0
-    const file = acceptedFiles[0]
+    const file = acceptedFiles[0] as File
 
     const fileUrl = URL.createObjectURL(file)
-    setSelectedFileUrl(fileUrl)
+    console.log(fileUrl)
+    setSelectedFileUrl(file)
+    // setSelectedFileUrl(fileUrl)
     onFileUploaded(file)
   }, [onFileUploaded])
 
@@ -32,7 +77,7 @@ const DropZone: React.FC<Props> = ({ onFileUploaded, text, accept }) => {
       <input {...getInputProps()} />
 
       {selectedFileUrl
-        ? <video src={selectedFileUrl} />
+        ? <div ref={webViewerRef} className="webViewer"></div>
         : (
           <P>
             <FiUpload />
